17a43c7f6e7ccfdd5ead1463b4f42d53654a63f1
Wladimir Braguini Domingues <wladimir.braguini@gmail.com>
2014-10-17 22:37:41 -0300

    add guard and update static_pages

--- Guardfile
+++ Guardfile
@@ -2,7 +2,7 @@
 # More info at https://github.com/guard/guard#readme
 require 'active_support/inflector'
 
-guard 'rspec', all_after_pass: false, cli: '--drb' do
+guard 'rspec', all_after_pass: false do
   watch(%r{^spec/.+_spec\.rb$})
   watch(%r{^lib/(.+)\.rb$})     { |m| "spec/lib/#{m[1]}_spec.rb" }
   watch('spec/spec_helper.rb')  { "spec" }
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,19 +3,23 @@ require 'spec_helper'
 describe "StaticPages" do
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the content 'Home Page'" do
       visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+      expect(page).to have_content('Home Page')
     end
-    it "should have the title 'Home'" do
+    it "should have the title base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
   describe "Help page" do
-    it "should have the content 'Help'" do
+    it "should have the content 'Help Page'" do
       visit '/static_pages/help'
-      expect(page).to have_content('Help')
+      expect(page).to have_content('Help Page')
     end
     it "should have the title 'Help'" do
       visit '/static_pages/help'
@@ -23,9 +27,9 @@ describe "StaticPages" do
     end
   end	
   describe "About page" do
-    it "should have the content 'About Us'" do
+    it "should have the content 'About Us Page'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About')
+      expect(page).to have_content('About Us Page')
     end
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
@@ -33,9 +37,9 @@ describe "StaticPages" do
     end
   end
   describe "Contact page" do
-    it "should have the content 'Contact'" do
+    it "should have the content 'Contact Page'" do
       visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
+      expect(page).to have_content('Contact Page')
     end
     it "should have the title 'Contact')" do
       visit '/static_pages/contact'
