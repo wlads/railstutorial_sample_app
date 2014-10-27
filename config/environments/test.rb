99e2dc617a506ef7137607743192ac0f757fa00e
Wladimir Braguini Domingues <wladimir.braguini@gmail.com>
2014-10-27 19:33:51 -0200

    finish user signup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,7 @@ SampleApp::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  # speed test
+  ActiveModel::SecurePassword.min_cost = true
 end
