7ca66507b8f6d8bc238f37490e6132ad5fec37dc
Wladimir Braguini Domingues <wladimir.braguini@gmail.com>
2014-10-27 23:29:00 -0200

    update helper to include gravatar parameters

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,11 @@
 module UsersHelper
 
-  def gravatar_for(user)
+  def gravatar_for(user, options = {})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url << "?size=#{options[:size]}" if options[:size]
+#    gravatar_url << "&d=#{options[:default]}" if options[:default]
+#    gravatar_url << "&d=mm" if options[:mm]
     image_tag(gravatar_url, alt:user.name, class: "gravatar")
   end
 
